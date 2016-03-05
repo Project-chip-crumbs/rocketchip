13fcfca812421f889db098bfd9fbab1540cf5157
Tina Zhao <tinayzhao@gmail.com>
2016-03-04 16:40:29 -0800

    styling

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,46 @@
 var $ = require('jquery');
 
 $(document).ready(function () {
+  // Switch
+  var switchBtnRight = $('.switch-button-case.right');
+  var switchBtnLeft = $('.switch-button-case.left');
+  var activeSwitch = $('.active');
+  // Bgs
+  var grounded = $('.grounded');
+  var spacedOut = $('.space');
 
-  $('.on-btn').click(function () {
-    $.ajax({
-      url: '/trigger/on',
-      type: 'POST',
-      success: function(response){
-        console.log('on');
-      }
-    });
-  });
+  function switchOff(){
+    switchBtnRight.removeClass('active');
+    switchBtnLeft.addClass('active');
+    spacedOut.hide();
+    grounded.show();
+  }
 
-  $('.off-btn').click(function () {
+  function switchOn(){
+    switchBtnRight.addClass('active');
+    switchBtnLeft.removeClass('active');
+    grounded.hide();
+    spacedOut.show();
+  }
+
+  spacedOut.hide();
+  
+  switchBtnLeft.on('click', function(){
     $.ajax({
       url: '/trigger/off',
       type: 'POST',
       success: function(response){
-        console.log('off');
+        switchOff();
+      }
+    });
+  });
+
+  switchBtnRight.on('click', function(){
+    $.ajax({
+      url: '/trigger/on',
+      type: 'POST',
+      success: function(response){
+        switchOn();
       }
     });
   });
